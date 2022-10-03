fc70ad87fe8c8213cb59ce58f461def94603f346
kaitlynnbeston1 <113719685+kaitlynnbeston1@users.noreply.github.com>
2022-10-03 09:25:09 -0600

    Here is the assignment.

---
+++ comment_practice.py
@@ -0,0 +1,4 @@
+# creating a variable to produce my favorite number in a print statement. 
+favorite_number=7*11
+# Printing my favorite number in a sentence, as expected in the assignment.
+print(f"My favorite number is {favorite_number}.")
